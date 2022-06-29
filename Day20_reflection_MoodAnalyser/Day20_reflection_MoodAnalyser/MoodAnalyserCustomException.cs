0a3ee6a2fc29a5a2408a1aa9ffc78c7f09e45af5
shreegowri <gaganandh@gmail.com>
2022-06-29 10:05:51 +0530

    [shreegowri]reflection with default constructor

---
+++ Day20_reflection_MoodAnalyser/Day20_reflection_MoodAnalyser/MoodAnalyserCustomException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day20_reflection_MoodAnalyser
+{
+    public class MoodAnalyserCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            EMPTY_MOOD,
+            NULL_MOOD,
+            NO_SUCH_CLASS,
+            NO_SUCH_METHOD,
+            EMPTY_MESSAGE,
+            NULL_MESSAGE,
+
+
+        }
+        public ExceptionType exceptionType;
+        public MoodAnalyserCustomException(ExceptionType exceptionType, string message) : base(message)
+        {
+            this.exceptionType = exceptionType;
+        }
+    }
+}
